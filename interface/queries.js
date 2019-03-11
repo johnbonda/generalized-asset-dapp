4b623eaa3ed528f2ea32277c1674b48773a7f7bc
johnbonda <john@belfricsbt.com>
2019-03-11 15:12:13 +0530

    Superuser queries

--- interface/queries.js
+++ interface/queries.js
@@ -112,3 +112,130 @@ app.route.post('/query/authorizers/pendingSigns', async function(req) {
         result: result.result
     }
 })
+
+app.route.post('/query/superuser/statistic/pendingIssues', async function(req){
+    var total = await new Promise((resolve)=>{
+        let sql = "select count(1) as total from issudepts where issudepts.deleted = '0';"
+        app.sideChainDatabase.get(sql, [], (err, row)=>{
+            if(err) resolve({
+                isSuccess: false,
+                message: JSON.stringify(err),
+                result: {}
+            });
+            resolve({
+                isSuccess: true,
+                result: row
+            });
+        });
+    });
+
+    if(!total.isSuccess) return total;
+
+    var result = await new Promise((resolve)=>{
+        let sql = "select issuers.email as issuerEmail, departments.name as department, count(issues.pid) as count from issuers join issudepts on issuers.iid = issudepts.iid join departments on issudepts.did = departments.did left join issues on issues.iid = issuers.iid and issues.status = 'authorized' where issuers.deleted = '0' and issudepts.deleted = '0' group by 1,2 order by 3 desc limit ? offset ?;"
+        app.sideChainDatabase.all(sql, [req.query.limit || 100, req.query.offset || 0], (err, row)=>{
+            if(err) resolve({
+                isSuccess: false,
+                message: JSON.stringify(err),
+                result: {}
+            });
+            resolve({
+                isSuccess: true,
+                result: row
+            });
+        });
+    });
+
+    if(!result.isSuccess) return result;
+
+    return {
+        isSuccess: true,
+        total: total.result.total,
+        result: result.result
+    }
+})
+
+
+app.route.post('/query/superuser/statistic/rejectedIssues', async function(req){
+    var total = await new Promise((resolve)=>{
+        let sql = "select count(1) as total from authdepts where authdepts.deleted = '0';"
+        app.sideChainDatabase.get(sql, [], (err, row)=>{
+            if(err) resolve({
+                isSuccess: false,
+                message: JSON.stringify(err),
+                result: {}
+            });
+            resolve({
+                isSuccess: true,
+                result: row
+            });
+        });
+    });
+
+    if(!total.isSuccess) return total;
+
+    var result = await new Promise((resolve)=>{
+        let sql = "select authorizers.email as authorizerEmail, departments.name as department, count(rejecteds.pid) as count from authdepts join authorizers on authdepts.aid = authorizers.aid join departments on authdepts.did = departments.did left join issues on issues.did = authdepts.did left join rejecteds on authdepts.aid = rejecteds.aid and rejecteds.pid = issues.pid where authdepts.deleted = '0' group by authorizers.email, departments.name limit ? offset ?;"
+        app.sideChainDatabase.all(sql, [req.query.limit || 100, req.query.offset || 0], (err, row)=>{
+            if(err) resolve({
+                isSuccess: false,
+                message: JSON.stringify(err),
+                result: {}
+            });
+            resolve({
+                isSuccess: true,
+                result: row
+            });
+        });
+    });
+
+    if(!result.isSuccess) return result;
+
+    return {
+        isSuccess: true,
+        total: total.result.total,
+        result: result.result
+    }
+})
+
+app.route.post('/query/superuser/statistic/pendingAuthorization', async function(req){
+    var total = await new Promise((resolve)=>{
+        let sql = "select count(1) as total from authdepts where authdepts.deleted = '0';"
+        app.sideChainDatabase.get(sql, [], (err, row)=>{
+            if(err) resolve({
+                isSuccess: false,
+                message: JSON.stringify(err),
+                result: {}
+            });
+            resolve({
+                isSuccess: true,
+                result: row
+            });
+        });
+    });
+
+    if(!total.isSuccess) return total;
+
+    var result = await new Promise((resolve)=>{
+        let sql = "select authorizers.email as authorizerEmail, departments.name as department, count(issues.pid) as count from authdepts join authorizers on authdepts.aid = authorizers.aid join departments on authdepts.did = departments.did left join issues on issues.authLevel = authdepts.level and authdepts.did = issues.did and issues.status = 'pending' and issues.pid where authdepts.deleted = '0' group by authorizers.email, departments.name limit ? offset ?;"
+        app.sideChainDatabase.all(sql, [req.query.limit || 100, req.query.offset || 0], (err, row)=>{
+            if(err) resolve({
+                isSuccess: false,
+                message: JSON.stringify(err),
+                result: {}
+            });
+            resolve({
+                isSuccess: true,
+                result: row
+            });
+        });
+    });
+
+    if(!result.isSuccess) return result;
+
+    return {
+        isSuccess: true,
+        total: total.result.total,
+        result: result.result
+    }
+})
