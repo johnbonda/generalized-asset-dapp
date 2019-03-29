e3651346d7b32cd9db204564ff4bac3458f615a6
johnbonda <john@belfricsbt.com>
2019-03-29 17:05:04 +0530

    Admin api

--- interface/belfricsAdmin.js
+++ interface/belfricsAdmin.js
@@ -1,12 +1,3 @@
-var BKVSCall = require('../utils/BKVSCall.js');
-var SuperDappCall = require("../utils/SuperDappCall");
-var DappCall = require("../utils/DappCall");
-var SwaggerCall = require('../utils/SwaggerCall.js');
-var logger = require("../utils/logger");
-var locker = require("../utils/locker");
-var blockWait = require("../utils/blockwait");
-var util = require("../utils/util");
-
 app.route.post('/admin/workDetails', async function(req){
     var issuersCount = await app.model.Issuer.count({
         deleted: '0'
